bead6e7ace001a7d1934c226df0c2bdcb474f804
Bianca Galindo <bianca.yamileth.galindo@outlook.com>
2023-03-03 23:53:05 -0600

    Practicas de orientación a objetos

---
+++ src/orientacionObjeto/CrearCuenta.java
@@ -0,0 +1,19 @@
+package orientacionObjeto;
+
+public class CrearCuenta {//clase que permitirá realizar procesos como por ejemplo "Crear una cuenta de banco"
+	public static void main ( String[] args) {
+		Cuenta         primeraCuenta = 			new Cuenta(); 
+//		Tipo de dato   Nombre de la variable    Datos que se asignaran
+		
+		primeraCuenta.saldo = 1000;
+//		Para acceder a los datos de la clase cuenta, es requerido utilizar la variable creada, la cual sirve como un puente para llegar la información
+//		de la clase Cuenta. 
+		
+		System.out.println(primeraCuenta.saldo);
+		
+		Cuenta segundaCuenta = new Cuenta();
+		
+		segundaCuenta.saldo=200;
+		System.out.println(segundaCuenta.saldo);
+	}
+}
---
+++ src/orientacionObjeto/Cuenta.java
@@ -0,0 +1,11 @@
+package orientacionObjeto;
+
+public class Cuenta { //Objeto, descrita como una clase.
+
+		double saldo; //Atributo
+		int agencia; //Atributo
+		int numero; //Atributo
+		String titular; //Atributo
+	
+
+}
