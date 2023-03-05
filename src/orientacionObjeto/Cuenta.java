df8fbb96557b54548e2eb31fa86cbf81b732390b
Bianca Galindo <bianca.yamileth.galindo@outlook.com>
2023-03-04 19:27:08 -0600

    Actualización de metodos

--- src/orientacionObjeto/Cuenta.java
+++ src/orientacionObjeto/Cuenta.java
@@ -1,15 +1,36 @@
 package orientacionObjeto;
 
-public class Cuenta { //Objeto, descrita como una clase.
+public class Cuenta { // Objeto, descrita como una clase.
 
-		double saldo; //Atributo
-		int agencia; //Atributo
-		int numero; //Atributo
-		String titular; //Atributo
-		//Atributos
-		
-		
-		void depositar (double valor) {
-			saldo=saldo + valor;
+	double saldo; // Atributo
+	int agencia; // Atributo
+	int numero; // Atributo
+	String titular; // Atributo
+	// Atributos
+
+	void depositar(double saldo) {
+		this.saldo = this.saldo + saldo;
+	}
+
+	public boolean retirar(double valor) {
+
+		if (this.saldo >= valor) {
+			this.saldo = this.saldo - valor;
+			return true;
+
+		} else {
+			return false;
 		}
 	}
+	public boolean transferir(double valor, Cuenta cuenta){
+		if(this.saldo >= valor) {
+			this.saldo = this.saldo -valor;
+			cuenta.depositar(valor);//cuenta.saldo = cuenta.saldo + valor; 
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	
+}
--- src/orientacionObjeto/Metodos.java
+++ src/orientacionObjeto/Metodos.java
@@ -1,12 +1,29 @@
 package orientacionObjeto;
 
 public class Metodos {
-public static void main(String[] args) {
-	Cuenta miCuenta = new Cuenta ();
-	miCuenta.saldo = 300;
-	miCuenta.depositar(200);
-	
-	System.out.println(miCuenta.saldo);
-}
+	public static void main(String[] args) {
+		boolean resultado;
+		Cuenta miCuenta = new Cuenta();
+		miCuenta.saldo = 300;
+		miCuenta.depositar(200);
+
+		System.out.println(miCuenta.saldo);
+		resultado = miCuenta.retirar(100);
+		if (resultado == true) {
+			System.out.println("Retiro de cuenta exitoso, su saldo es : " + miCuenta.saldo);
+
+		} else {
+			System.out.println("El retiro de cuenta no fue completado");
+		}
+		
+		Cuenta cuentaDeJimena = new  Cuenta();
+		cuentaDeJimena.depositar(1000);
+		if(cuentaDeJimena.transferir(400, miCuenta)) {
+			
+			System.out.println("La tranferencia fue exitosa el saldo de jimena es : " + cuentaDeJimena.saldo);
+			System.out.println("El saldo de mi cuenta es: "+ miCuenta.saldo);
+		}
+
+	}
 
 }
